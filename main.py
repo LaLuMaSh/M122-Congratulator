29d2e6b268475f0143a61e2638fecef768209eda
Luca Güttinger <38906805+Luca-Guettinger@users.noreply.github.com>
2021-06-01 01:31:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,26 +6,20 @@ from datetime import datetime
 import BirthdayMailManagement
 from BirthdayFileManagement import get_birth_dates_today
 
-def runApp() -> bool:
-    if len(sys.argv) != 2:
-        exit(1212)
-        return False
+if len(sys.argv) != 2:
+    exit(1212)
 
-    configPath = sys.argv[1]
-    if configPath.startswith('json'):
-        exit(213123)
-        return False
+configPath = sys.argv[1]
+if configPath.startswith('json'):
+    exit(213123)
 
-    with open(configPath) as config_file:
-        config = json.load(config_file)
-    logging.basicConfig(filename=config['logfile'], level=logging.DEBUG)
+with open(configPath) as config_file:
+    config = json.load(config_file)
+logging.basicConfig(filename=config['logfile'], level=logging.DEBUG)
 
-    logging.info(datetime.now().strftime("%H:%M:%S") + " starting Congratulator...")
-    to_congratulate = get_birth_dates_today(config)
+logging.info(datetime.now().strftime("%H:%M:%S") + " starting Congratulator...")
+to_congratulate = get_birth_dates_today(config)
 
-    for cong in to_congratulate:
-        BirthdayMailManagement.send_mail(config, cong)
-    logging.info(datetime.now().strftime("%H:%M:%S") + " Congratulator: Done")
-    return True
-
-runApp()
+for cong in to_congratulate:
+    BirthdayMailManagement.send_mail(config, cong)
+logging.info(datetime.now().strftime("%H:%M:%S") + " Congratulator: Done")
